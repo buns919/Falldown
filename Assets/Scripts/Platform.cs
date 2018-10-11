fa189a0b29fe080ed15fda91ac74043877c34d82
buns919 <sbehun07+1@gmail.com>
2018-10-10 23:31:43 -0400

    created basic background, player and platforms

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour {
+
+    [SerializeField] float verticleMoveSpeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 moveVector = Vector3.up * (Time.deltaTime * verticleMoveSpeed);
+        transform.Translate(moveVector, Space.World);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] float moveSpeed = 100f;
+
+    private Rigidbody rb;
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() {
+        MoveHorizontal();
+    }
+
+    private void MoveHorizontal() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(moveHorizontal, 0f, 0f);
+        rb.AddForce(movement * moveSpeed);
+    }
+
+}
