00ef0828d1ffd20a31c1b50a5b0a17f75a2fcae4
buns919 <sbehun07+1@gmail.com>
2018-10-11 23:33:59 -0400

    saving WIP on spawner

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -4,13 +4,60 @@ using UnityEngine;
 
 public class PlatformSpawner : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] float secondsBetweenRowSpawns = 4f;
+    [SerializeField] GameObject platformPrefab;
+
+    private bool enableWaveSpawn = true;
+
+    // Use this for initialization
+    private IEnumerator Start() {
+        // start spawing waves until we're told to stop
+        do {
+            yield return StartCoroutine(SpawnRow());
+        }
+        while (enableWaveSpawn);
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+
+
+    IEnumerator SpawnRow() {
+        int numSpawns = 0;
+
+        Transform[] waypoints = GetComponentsInChildren<Transform>();
+        List<GameObject> instances = new List<GameObject>();
+
+        //foreach (Transform waypoint in waypoints) {
+        for (int i = 0; i < waypoints.Length; i++) {
+
+            if (Random.value > 0.4f) {
+                var instance = Instantiate(platformPrefab, waypoints[i].transform) as GameObject;
+                instance.transform.parent = gameObject.transform.parent;
+                instances.Add(instance);
+                numSpawns++;
+            }
+        }
+
+        if (numSpawns == waypoints.Length) {
+            Destroy(instances[Random.Range(0, waypoints.Length - 1)]);
+        }
+
+        yield return new WaitForSeconds(secondsBetweenRowSpawns);
+    }
+
+    IEnumerator OldSpawnRow() {
+        var spawnTransform = GetComponentInChildren<Transform>().transform;
+        GameObject platformInstance = Instantiate(platformPrefab, spawnTransform) as GameObject;
+
+        // create a random width
+        Vector3 currentScale = platformInstance.transform.localScale;
+        float randomRange = Random.Range(currentScale.x * .25f, currentScale.x * 1.75f);
+        platformInstance.transform.localScale = new Vector3(randomRange, currentScale.y, currentScale.z);
+
+        // adjust the starting position
+        //platformInstance.transform.position = new Vector3();
+        yield return new WaitForSeconds(secondsBetweenRowSpawns);
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour {
     [SerializeField] float moveSpeed = 100f;
 
     private Rigidbody rb;
+    private bool isFalling = false;
 	
 	// Update is called once per frame
 	void Update () {
@@ -20,12 +21,36 @@ public class Player : MonoBehaviour {
 
     void FixedUpdate() {
         MoveHorizontal();
+
+        /*
+        // if we are not falling and on a platform, apply the horizontal force
+        if (!isFalling) {
+            MoveHorizontal();
+        }
+        // try to limit the horizontal momentum when falling so we fall more straight down
+        else if (rb.velocity.x > 1.5f) {
+            rb.velocity = new Vector3(rb.velocity.x * .8f, rb.velocity.y, 0f);
+        }
+        */
     }
 
     private void MoveHorizontal() {
         float moveHorizontal = Input.GetAxis("Horizontal");
         Vector3 movement = new Vector3(moveHorizontal, 0f, 0f);
         rb.AddForce(movement * moveSpeed);
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == "Platform") {
+            isFalling = false;
+        }
+
+    }
+
+    private void OnCollisionExit(Collision collision) {
+        if (collision.gameObject.tag == "Platform") {
+            isFalling = true;
+        }
     }
 
 }
